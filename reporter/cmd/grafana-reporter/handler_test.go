1f8682e536ffb35b226a2cfad6e0c32456bbbeda
Kiran Basuti <106589360+kirus3734@users.noreply.github.com>
2024-05-29 14:01:13 +0530

    Update handler_test.go

--- reporter/cmd/grafana-reporter/handler_test.go
+++ reporter/cmd/grafana-reporter/handler_test.go
@@ -25,8 +25,8 @@ import (
 	"net/url"
 	"testing"
 
-	"github.com/IzakMarais/reporter/grafana"
-	"github.com/IzakMarais/reporter/report"
+	"github.com/kirus3734/grafana-renderer/reporter/grafana"
+	"github.com/kirus3734/grafana-renderer/reporter/report"
 	"github.com/gorilla/mux"
 	. "github.com/smartystreets/goconvey/convey"
 )
